cfa44476c4b75df1861b63cc1062ef80d57ae61a
jessicakay <52629990+jessicakay@users.noreply.github.com>
2020-04-07 17:19:40 -0400

    updates

--- datasets/populations.R
+++ datasets/populations.R
@@ -1,28 +1,16 @@
 # quick script to import capacity data
 #
-cap<-read.csv("~/../Documents/GitHub/inspectionbrowser/datasets/capacities.csv",header=TRUE)
-
+cap<-read.csv("~/../Documents/GitHub/inspectionbrowser/datasets/MA_capacities_cleaned.csv",header=TRUE)
 cap$Facility<-tolower(cap$Facility)
 cap$Facility<-gsub("-"," ",cap$Facility)
 
-fList<-c("mci norfolk","souza","worcester county", "norfolk county", "essex county", "dartmouth",
-         "cedar junction", "mci walpole", "treatment center", "northeastern correctional",
-         "north central", "bay state correctional", "dartmouth women", "dukes county",
-         "souza","south middlesex correction","mci concord", "plymouth county", "old colony",
-         "pondville","middlesex county","shirley","mci framingham", "mci plymouth",
-         "berkshire county","barnstable county","suffolk county", "boston pre-release",
-         "bridge water", "bridgewater","bristol county","hampden county", 
-         "boston pre release", "old colony")
-
-
-i<-1
-iNum<-1
-for(n in cap$Facility){
-  for(f in fList){
-    if(str_detect(cap$Facility[i],fList[iNum])==TRUE){
-      cap$Facility[i]<-fList[iNum]
-    }
-    iNum<-iNum+1
-  }
-  i<-i+1
+for(f in cap$Facility){
+  ds$capacity[ds$facility==f]<-as.numeric(sum(cap$Capacity[cap$Facility==f]))
 }
+
+
+ds$over_cap<-ifelse(!is.na(ds$capacity)==TRUE,ds$total_pop-ds$capacity,"")
+
+
+View(ds %>% select(facility, total_pop, capacity,max_capacity))
+
