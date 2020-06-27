e6b69406f340c04c711c04a3166c4c6b6b5ffd19
jessicakay <52629990+jessicakay@users.noreply.github.com>
2020-06-27 18:26:18 -0400

    grabber.py

---
+++ datasets/grab.py
@@ -0,0 +1,26 @@
+''' github.com/jessicakay '''
+
+import os
+import re
+
+home = os.path.expanduser('~')
+dtop=''.join([home,"/Desktop/"])
+
+def grabber():
+	reportlist = input("url list location:")
+	engine = input("use wget [w] or curl [c]:")
+	with open(reportlist, 'r') as lst:
+		reports=lst.readlines()
+	for i in reports:
+		i=i.rstrip()
+		x=i[25:len(i)-9]
+		print(x)
+		z=''.join(re.findall(r'[A-Za-z0-9]+',x))
+		print("downloading... ",z)
+		if engine=="w" or engine=="":
+			os.system(''.join(["wget -O",z," \"",i,"\""]))
+		elif engine=="c":
+			os.system(''.join(["curl -O -J -L ", i]))
+		d=input("next?")
+		if d == 'q':
+			quit()
