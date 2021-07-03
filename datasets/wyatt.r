8bbb27867ccc87303ec0592f4e9fb7fedc9fc411
jessicakay <52629990+jessicakay@users.noreply.github.com>
2021-07-03 14:09:03 -0400

    Update wyatt.r

--- datasets/wyatt.r
+++ datasets/wyatt.r
@@ -10,7 +10,7 @@ library(pdftools)
 library(dplyr)
 library(stringr)
 
-setwd("~/Downloads/wyatt/")
+setwd("/Users/jessa/Downloads/wyatt/")
 
 local_files <-list.files()
 
@@ -20,8 +20,8 @@ readOCR <- function(target){
   pdftools::pdf_ocr_text(target, pages = 1) ->> trg
   targ<<-str_split(trg, pattern="\n") %>% as.array()
   as.array(str_extract(targ[[1]][1],"[0-9]+/[0-9]+/[0-9]+")) -> newDate
-  as.array(str_extract(targ[[1]][c(7,12,13,20)],"[0-9]+")) -> newObs      # vector values are 
-  newObs <<- append(newDate,newObs)                                       # positions in array
+  as.array(str_extract(targ[[1]][c(7,12,13,20)],"[0-9]+")) -> newObs          # vector values are 
+  newObs <<- append(newDate,newObs)                                           # positions in array
 }
 
 n<-1
@@ -36,36 +36,43 @@ for(l in local_files){if(n<2){net<-as.data.frame(NULL)}
 
 # added benchmarking
 
-readOCR <- function(target){
+
+readOCR <- function(target,gridVals){
   startT<-Sys.time()
   pdftools::pdf_ocr_text(target, pages = 1) ->> trg
   targ<<-str_split(trg, pattern="\n") %>% as.array()
   as.array(str_extract(targ[[1]][1],"[0-9]+/[0-9]+/[0-9]+")) -> newDate
-  as.array(str_extract(targ[[1]][c(7,12,13,20)],"[0-9]+")) -> newObs
+  as.array(str_extract(targ[[1]][setTarg],"[0-9]+")) -> newObs
   newObs <<- append(newDate,newObs)
   endT<-Sys.time()
   flush.console()
   writeLines(paste(round(eval(endT-startT),2)," secs to process\n"))
 }
 
-n<-1
-for(l in local_files){
-  if(n<2){
-    net<-as.data.frame(NULL)
-    benchStart<-Sys.time()}
-  flush.console()
-  if(grepl(".pdf",l)){
-    writeLines(paste("processing file ",n,": ",l,"...\n"))
-    readOCR(l)
-    rbind(net,newObs) ->> net}
-  if(n==length(local_files)){
-    writeLines(paste(round(eval((Sys.time()-benchStart)),2)," mins runtime\n."))
+runScan<-function(gridVals=c(7,12,13,20)){
+  n<-1
+  for(l in local_files){
+    if(n<2){
+      net<-as.data.frame(NULL)
+      benchStart<-Sys.time()}
+    flush.console()
+    if(grepl(".pdf",l)){
+      writeLines(paste("processing file ",n,": ",l,"...\n"))
+      readOCR(l,gridVals)
+      rbind(net,newObs) ->> net}
+    if(n==length(local_files)){
+      writeLines(paste(round(eval((Sys.time()-benchStart)),2)," mins runtime\n."))
+    }
+    n<-n+1
   }
-  n<-n+1
 }
 
+  # run the scan with default, and custom peramaeters
 
-# asd
+  runScan()                              # run with default perameters
+  runScan(gridVals = c(1,6,9))           # extract numeric data from array positions 1, 6, and 9
+
+# scratchpad, comparing ocr_data to ocr_tex
 
 as.data.frame(pdf_ocr_data("050420report.pdf",pages = 1)) -> x
 as.data.frame(pdf_ocr_data("june8th.pdf",pages = 1)) -> y
