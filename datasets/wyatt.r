8e8bffd05db4226aa2cad3853ac11dd65a367a17
jessicakay <52629990+jessicakay@users.noreply.github.com>
2021-07-03 13:51:47 -0400

    Update wyatt.r

--- datasets/wyatt.r
+++ datasets/wyatt.r
@@ -11,6 +11,7 @@ library(dplyr)
 library(stringr)
 
 setwd("~/Downloads/wyatt/")
+
 local_files <-list.files()
 
 # core code
@@ -19,14 +20,14 @@ readOCR <- function(target){
   pdftools::pdf_ocr_text(target, pages = 1) ->> trg
   targ<<-str_split(trg, pattern="\n") %>% as.array()
   as.array(str_extract(targ[[1]][1],"[0-9]+/[0-9]+/[0-9]+")) -> newDate
-  as.array(str_extract(targ[[1]][c(7,12,13,20)],"[0-9]+")) -> newObs
-  newObs <<- append(newDate,newObs)
+  as.array(str_extract(targ[[1]][c(7,12,13,20)],"[0-9]+")) -> newObs      # vector values are 
+  newObs <<- append(newDate,newObs)                                       # positions in array
 }
 
 n<-1
 for(l in local_files){if(n<2){net<-as.data.frame(NULL)}
   if(grepl(".pdf",l)){
-    writeLines(paste("processing... ",l,"\n"))
+    writeLines(paste("processing file ",n,": ",l,"...\n"))
     readOCR(l)
     rbind(net,newObs) ->> net 
     }
@@ -35,33 +36,42 @@ for(l in local_files){if(n<2){net<-as.data.frame(NULL)}
 
 # added benchmarking
 
-
 readOCR <- function(target){
   startT<-Sys.time()
   pdftools::pdf_ocr_text(target, pages = 1) ->> trg
-  midT<-Sys.time()
   targ<<-str_split(trg, pattern="\n") %>% as.array()
   as.array(str_extract(targ[[1]][1],"[0-9]+/[0-9]+/[0-9]+")) -> newDate
   as.array(str_extract(targ[[1]][c(7,12,13,20)],"[0-9]+")) -> newObs
   newObs <<- append(newDate,newObs)
   endT<-Sys.time()
   flush.console()
-  writeLines(eval(startT-midT),eval(startT-endT))
+  writeLines(paste(round(eval(endT-startT),2)," secs to process\n"))
 }
 
 n<-1
 for(l in local_files){
-  if(n<2){net<-as.data.frame(NULL)}
+  if(n<2){
+    net<-as.data.frame(NULL)
+    benchStart<-Sys.time()}
   flush.console()
   if(grepl(".pdf",l)){
-    writeLines(paste("processing... ",l,"\n"))
+    writeLines(paste("processing file ",n,": ",l,"...\n"))
     readOCR(l)
-    rbind(net,newObs) ->> net
+    rbind(net,newObs) ->> net}
+  if(n==length(local_files)){
+    writeLines(paste(round(eval((Sys.time()-benchStart)),2)," mins runtime\n."))
   }
   n<-n+1
 }
 
 
+# asd
+
+as.data.frame(pdf_ocr_data("050420report.pdf",pages = 1)) -> x
+as.data.frame(pdf_ocr_data("june8th.pdf",pages = 1)) -> y
+as.data.frame(pdf_ocr_data("wdc2.pdf",pages = 1)) -> z
 
 
-
+x[,1][45]
+y[,1][45]
+z[,1][45]
